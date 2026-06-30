26774624a4e635bcbe368efd0de1e09bad1ecb6f
Sebastian Esses Nobre <seba.esses@gmail.com>
2026-06-30 15:56:38 -0300

    feat: implementa criacao de tabela e teste de insercao de medico

--- crud_medicos.py
+++ crud_medicos.py
@@ -1,21 +1,29 @@
-import panel as pn
-import pandas as pd
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
+from urllib.parse import quote_plus
 
-pn.extension('tabulator')
+senha = quote_plus("sebaesses123")
+db_url = f'postgresql+pg8000://postgres:{senha}@127.0.0.1:5432/hospital'
 
-engine = create_engine('postgresql+psycopg2://postgres:sebaesses123@localhost:5432/hospital')
+engine = create_engine(db_url)
 Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
 
 class Medico(Base):
     __tablename__ = 'medicos'
-    
     id = Column(Integer, primary_key=True, autoincrement=True)
     nome_completo = Column(String, nullable=False)
     crm = Column(String, unique=True, nullable=False)
     especialidade = Column(String)
 
 Base.metadata.create_all(engine)
+print("Tabela 'medicos' criada com sucesso!")
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+novo_medico = Medico(nome_completo="Dr. Teste", crm="12345", especialidade="Clínico Geral")
+
+session.add(novo_medico)
+session.commit()
+print("Médico inserido com sucesso!")
